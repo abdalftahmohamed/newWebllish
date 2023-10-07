d553581e43db76226235d06023dcba00177b01b7
User Name <Email@example.com>
2023-10-07 12:11:01 +0300

    "ff"

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,26 +2,20 @@
 
 namespace App\Models;
 
-use Laratrust\Contracts\LaratrustUser;
-use Laratrust\Traits\HasRolesAndPermissions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Support\Str;
-class Team extends Authenticatable implements LaratrustUser
+
+class Team extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRolesAndPermissions;
+    use HasApiTokens, HasFactory, Notifiable;
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'namesection',
-        'description',
-    ];
+    protected $guarded=[];
 
     // protected $hidden=['image'];
 //    protected $appends=[ 'image_url' ];
